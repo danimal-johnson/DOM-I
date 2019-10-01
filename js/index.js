e715e94d702d8f71aab9c856aa0461a858e3719a
Dan Johnson <danimal.johnson@gmail.com>
2019-09-30 18:50:28 -0700

    Images and navbar

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ const siteContent = {
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
+    "nav-item-7": "Catalog",
+    "nav-item-8": "Sale!",
     "img-src": "img/logo.png"
   },
   "cta": {
@@ -37,6 +39,92 @@ const siteContent = {
   },
 };
 
+
+// ------------- Images ----------------
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let ctaImage = document.getElementById("cta-img");
+ctaImage.setAttribute("src", siteContent["cta"]["img-src"]);
+
+let midImg = document.getElementById("middle-img");
+midImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+
+const headerImage = document.querySelector("h1");
+headerImage.textContent = "DOM Is Awesome"
+
+
+// ------------ Navigation -----------------
+// let nav1 = document.getElementsByTagName()
+// let myH1 = document.getElementsByTagName("h1");
+// myH1.innerHTML = "This is an H1 tag";
+
+// This works //
+let navBar = document.querySelector("nav");
+// navBar.setAttribute("style", "color: green;");
+// let joe = document.getElementsByClassName("nav").style.color = "hotpink";
+// navBar.style.color = "hotpink";
+// let newTextNode = document.createTextNode("One");
+// navBar.appendChild("newTextNode");
+// newTextNode = document.createTextNode("One");
+
+
+
+// let navAnchors = navBar.getElementsByTagName("a");
+// console.log(navAnchors);
+
+let navItem = document.querySelectorAll("a");
+
+var node1 = document.createElement("a");
+var textnode1 = document.createTextNode(siteContent["nav"]["nav-item-7"]);
+node1.appendChild(textnode1);
+navBar.appendChild(node1);
+
+var node2 = document.createElement("a");
+var textnode2 = document.createTextNode(siteContent["nav"]["nav-item-8"]);
+node2.appendChild(textnode2);
+navBar.appendChild(node2);
+
+navItem = document.querySelectorAll("a"); // Make the list longer now
+
+for (let i=0; i < navItem.length; i++) {
+  navItem[i].style.color = "green";
+}
+
+console.log(navItem);
+
+
+
+
+navItem[5].style.color = "green"; // FG color doesn't work
+navItem[0].textContent = siteContent["nav"]["nav-item-1"]; // Put on each element
+navItem[1].textContent = siteContent["nav"]["nav-item-2"];
+navItem[2].textContent = siteContent["nav"]["nav-item-3"];
+navItem[3].textContent = siteContent["nav"]["nav-item-4"];
+navItem[4].textContent = siteContent["nav"]["nav-item-5"];
+navItem[5].textContent = siteContent["nav"]["nav-item-6"];
+// navItem[6].textContent = siteContent["nav"]["nav-item-7"];
+// navItem[7].textContent = siteContent["nav"]["nav-item-8"];
+
+// navItem.forEach((item, index)) => (item.textContent = Object.values[siteContent.nav[index]];
+console.log(navItem);
+ 
+
+
+
+
+
+
+// mySelection.appendChild("newTextNode");
+
+
+
+// document.getElementById("title").innerHTML = "W3Schools";
+// document.getElementById("title").href = "https://www.w3schools.com";
+// document.getElementById("m").target = "_blank";
+
+
+// let myContactInfo = document.getElementById("contact");
+// let myh4 = myContactInfo.getElementByTagName(h4).innerHTML = "I can't believe this isn't working";
